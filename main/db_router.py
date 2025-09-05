cb8104a17445eb11210996b8d9d0497341b9e03d
Innotexco <53158382+Innotexco@users.noreply.github.com>
2025-09-05 08:56:09 +0100

    Add files via upload

---
+++ main/db_router.py
@@ -0,0 +1,46 @@
+from django.db import connections
+import os
+
+
+def add_db_connection( db_name):
+    if db_name in connections.databases:  # Prevent duplicate connections
+        # print('exist', db_name)
+        return
+    # print("////", db_name)
+    connections.databases[db_name] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": db_name,  # Dynamic database name
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': '5432',
+        'TIME_ZONE' : 'UTC',
+        "OPTIONS": {"options": "-c timezone=UTC"},
+        "CONN_HEALTH_CHECKS": False,
+        "CONN_MAX_AGE": 0,
+        "AUTOCOMMIT": True, 
+        "ATOMIC_REQUESTS": False,   
+    }
+
+
+from django.conf import settings
+from Stockin.models import company_table
+from main.middleware import get_tenant_db
+
+class MultiTenantRouter:
+    """Database router to dynamically switch between tenant databases."""
+
+    def db_for_read(self, model, **hints):
+        """Get the tenant database from thread-local storage."""
+        db_name = get_tenant_db()
+    
+        return db_name if db_name in settings.DATABASES else "default"
+
+    def db_for_write(self, model, **hints):
+        """Use the same logic for writing."""
+        return self.db_for_read(model, **hints)
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """Only allow migrations on the default database."""
+        return db == "default"  # Prevents accidental migrations on tenant DBs
+
