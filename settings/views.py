db30730c2690f37d90f73930ea98624706ed1d11
futuregodwin <164335576+futuregodwin@users.noreply.github.com>
2026-01-15 11:26:59 +0100

    Import connections from django.db in views.py

--- settings/views.py
+++ settings/views.py
@@ -447,6 +447,8 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+ from django.db import connections
+
 
 @login_required
 @require_http_methods(["GET"])
