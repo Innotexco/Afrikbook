7f0ab8f06519d7addf4ec624f8469fa67581c5bb
futuregodwin <godwinfuture067@gmail.com>
2026-05-14 14:19:46 +0100

    warehouse  stock report

--- Stock/Forms/stockTransferForm.py
+++ Stock/Forms/stockTransferForm.py
@@ -4,7 +4,7 @@ from Stock.utils import random_string_generator;
 class W_W_Form(forms.ModelForm):
     class Meta:
         model = CreateStockInLog
-        fields = ['supplier', 'warehouse', 'description', 'item_decription', 'item', 'quantity', 'outlet', 'source', 'token_id', 'item_code', 'ref_no', 'Userlogin', 'selling_price', 'datetx']
+        fields = ['supplier', 'warehouse', 'description', 'item_decription', 'item', 'quantity', 'outlet', 'source', 'token_id', 'item_code', 'ref_no', 'Userlogin', 'selling_price']
         widgets = {
          
           'Userlogin': forms.HiddenInput(attrs={'value': 'admin'}),
@@ -12,7 +12,6 @@ class W_W_Form(forms.ModelForm):
           'source': forms.HiddenInput(attrs={'value': 'vendor'}),
           'description': forms.Textarea(attrs={'class': 'w-full border rounded py-2 px-3', 
                'placeholder':"Enter Description",}),
-          'datetx': forms.DateInput(attrs={'class': 'w-full border rounded py-2 px-3', 'type': 'date'}),
           'item_decription[]': forms.TextInput(attrs={ 'required': 'False'} ),
           'item_code[]': forms.TextInput(attrs={ 'required': 'False'} ),
           'item[]': forms.TextInput(attrs={ 'required': 'False'} ),
@@ -29,7 +28,7 @@ class W_W_Form(forms.ModelForm):
 class W_O_Form(forms.ModelForm):
   class Meta:
       model = CreateOutletStockInLog
-      fields = ['supplier', 'warehouse', 'description', 'item_decription', 'item', 'quantity', 'outlet',  'token_id', 'item_code', 'ref_no', 'Userlogin', 'selling_price', 'wholesale_price', 'datetx']
+      fields = ['supplier', 'warehouse', 'description', 'item_decription', 'item', 'quantity', 'outlet',  'token_id', 'item_code', 'ref_no', 'Userlogin', 'selling_price', 'wholesale_price']
       randomtoken = random_string_generator()
       widgets = {
         'token_id': forms.HiddenInput(attrs={'value': 'Token_'+randomtoken}),
@@ -37,7 +36,6 @@ class W_O_Form(forms.ModelForm):
         'supplier': forms.HiddenInput(attrs={'value': 'vendor'}),
         'description': forms.Textarea(attrs={'class': 'w-full border rounded py-2 px-3', 
               'placeholder':"Enter Description",}),
-        'datetx': forms.DateInput(attrs={'class': 'w-full border rounded py-2 px-3', 'type': 'date'}),
         'item_decription[]': forms.TextInput(attrs={ 'required': 'False'} ),
         'item_code[]': forms.TextInput(attrs={ 'required': 'False'} ),
         'item[]': forms.TextInput(attrs={ 'required': 'False'} ),
